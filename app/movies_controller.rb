00884924278ccd1634a08d46047f7ed230b4d54b
Evan Candler <emcandler21@gmail.com>
2018-04-05 02:26:50 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -69,7 +69,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  movie = Movie.find_by(title: "Wat?")
+  movie = Movie.find_by(title: 'Wat?')
   movide.update(title: 'Wat, huh?')
 end
 
